4524ea0ecbf2d08fbbc6b35a520be33035cbf100
veeksha45 <veeksha45@gmail.com>
2025-06-25 22:44:13 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// Write your code below:
+const a = document.getElementById('main-heading');
+a.style.backgroundColor = "green";
+a.style.color ="orange";
+a.textContent = "Fruit World";
+
+
+const b = document.getElementById('header');
+b.style.backgroundColor = "green";
+b.style.borderBottom = "3px solid orange";
+
+
+const c = document.getElementById('basket-heading');
+c.style.color = "green";
+
+const d = document.getElementById('thanks');
+d.innerHTML="<p>Please visit us again</p>"
+
+
+
+
+
+
